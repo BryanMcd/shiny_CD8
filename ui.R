4d8957657c468fb58fb2bc41534d7d7defadbbf7
BryanMcd <bryanmcd92@gmail.com>
2020-06-14 15:33:23 -0700

    Update ui.R

--- ui.R
+++ ui.R
@@ -5,13 +5,10 @@ ui <- fluidPage(
     sidebarPanel(
       h4("GSE94964 & GSE106420 Merged"),
       br(),
-      
       passwordInput("password", "Enter Password:", value = ""),
       actionButton("go", "Submit"),
       verbatimTextOutput("value"),
-      
       br(),
-      #textInput(inputId = "Gene", label = "Gene Input", width = "150px", value = "ICOS"),
       selectInput(inputId = "Gene", label = "Gene Input", 
                   choices = levels(counts$variable), 
                   selected = "ICOS", 

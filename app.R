2e43eb9a72050e9866afea8484474179c059887d
BryanMcd <bryanmcd92@gmail.com>
2020-06-14 15:48:25 -0700

    Update app.R

--- app.R
+++ app.R
@@ -25,32 +25,4 @@ server <- function(input, output) {
     })
 }
 
-
-ui <- fluidPage(
-  titlePanel("Gene Expression in Human CD8 T cells"),
-  
-  sidebarLayout(
-    sidebarPanel(
-      h4("GSE94964 & GSE106420 Merged"),
-      br(),
-      passwordInput("password", "Enter Password:", value = ""),
-      actionButton("go", "Submit"),
-      verbatimTextOutput("value"),
-      br(),
-      selectInput(inputId = "Gene", label = "Gene Input", 
-                  choices = levels(counts$variable), 
-                  selected = "ICOS", 
-                  multiple = FALSE,
-                  selectize = TRUE, 
-                  width = NULL, 
-                  size = NULL),
-      submitButton("Update Plot")
-      ),
-    
-    mainPanel(
-      plotOutput("counts")
-      )
-    )
-)
-
 shinyApp(ui = ui, server = server)

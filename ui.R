f5d14d6ac1db64edd6b4049af4efeaffe5b28d57
BryanMcd <bryanmcd92@gmail.com>
2020-06-14 15:11:54 -0700

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,8 +1,13 @@
 ui <- fluidPage(
   titlePanel("Gene Expression in Human CD8 T cells"),
+  
   sidebarLayout(
     sidebarPanel(
       h4("GSE94964 & GSE106420 Merged"),
+      br(),
+      passwordInput("password", "Enter Password:", value = ""),
+      actionButton("go", "Submit"),
+      verbatimTextOutput("value"),
       br(),
       textInput(inputId = "Gene", label = "Gene Input", width = "150px", value = "ICOS"),
       submitButton("Update Plot")
